742df4bf59b76fe87be39334d1acbbaaae92ebfb
Yusux <syusux@gmail.com>
2024-02-18 23:01:26 +0800

    feat: init commit to add WebSocket Receiver

---
+++ README.md
@@ -0,0 +1,61 @@
+<style>
+@font-face {
+    font-family: "iconfont";
+    src: url("https://raw.githubusercontent.com/Yusux/WebSocket-Receiver/main/font/iconfont.ttf") format("truetype");
+}
+
+.iconfont {
+    font-family: "iconfont" !important;
+    font-size: 1rem;
+    font-style: normal;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+</style>
+
+<div align="center">
+    <h3 align="center">WebSocket Receiver</h3>
+    <p align="center">
+        A WebSocket client to receive messages from a server using JavaScript and HTML.
+        <br />
+        <br />
+        <a href="https://lab.yusux.xyz/WebSocket-Receiver">View Demo</a>
+        ·
+        <a href="https://github.com/Yusux/WebSocket-Receiver/issues">Report Bug</a>
+        ·
+        <a href="https://github.com/Yusux/WebSocket-Receiver/issues">Request Feature</a>
+    </p>
+</div>
+
+## About The Project
+
+Since I found it difficult to find a good WebSocket client that only uses html and only receives messages, I implemented this. The main purpose is to implement a client for [Korabli-chatrans](https://github.com/Yusux/Korabli-chatrans).
+
+### Built With
+
+[![Static Badge](https://img.shields.io/badge/Bootstrap-563D7C?logo=bootstrap&logoColor=white)](https://getbootstrap.com/)
+[![Static Badge](https://img.shields.io/badge/jQuery-0769AD?logo=jquery&logoColor=white)](https://jquery.com)
+
+## Getting Started
+
+The css and javascript files are in the css and js folders respectively. The html file is in the root folder named `index.html`. Just open the html file in a browser and you are good to go.
+
+### Usage
+
+1. Open the `index.html` file in a browser.
+2. Click the <span id="configtext" class="icon iconfont">&#xe7f9;</span> button on the top right corner to open the configuration modal.
+3. Input the server address and port in the input fields.
+4. Click the `Connect` button to connect to the server.
+5. If the connection is successful, the modal will close after about 2 seconds and the messages will start to appear in the message box.
+
+|Config Example|Receiving Example|
+|:-:|:-:|
+|![Config Example](img/config_example.png)|![Receiving Example](img/receiving_example.png)|
+
+## Contributing
+
+Any contributions you make are **greatly appreciated**. You can fork the project and make a pull request. Also, you can open an issue if you find a bug or want to request a feature.
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
---
+++ js/customscript.js
@@ -0,0 +1,118 @@
+// theme changer
+theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'light' : 'dark';
+function themeChange() {
+    theme = theme == 'dark' ? 'light' : 'dark';
+    if (theme == 'dark') {
+        $("body").removeClass("text-bg-light");
+        $("body").addClass("text-bg-dark");
+        $("a").css("color", "#fff");
+        $(".modal-content").removeClass("text-bg-light");
+        $(".modal-content").addClass("text-bg-dark");
+        $(".btn-close").addClass("btn-close-white");
+        $("#modetext").html("&#xe60f;");
+    } else {
+        $("body").removeClass("text-bg-dark");
+        $("body").addClass("text-bg-light");
+        $(".modal-content").removeClass("text-bg-dark");
+        $(".modal-content").addClass("text-bg-light");
+        $("a").css("color", "#000");
+        $(".btn-close").removeClass("btn-close-white");
+        $("#modetext").html("&#xe6c0;");
+    }
+}
+themeChange();
+
+// hover effect for links
+$("a").hover(
+    function () {
+        $(this).css("color", "#F0C9CF");
+    },
+    function () {
+        if (theme == 'dark') {
+            $(this).css("color", "#fff");
+        } else {
+            $(this).css("color", "#000");
+        }
+    }
+);
+
+
+// alert message
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    alertPlaceholder.innerHTML = '';
+    alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('connect')
+if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+        // check if the alert is already visible
+
+        // connect to the server according to the input
+        var host = document.getElementById("host").value;
+        var port = document.getElementById("port").value;
+        connect(host, port);
+
+        // alert message
+        appendAlert('Connecting to ' + host + ':' + port, 'info');
+    })
+}
+
+// WebSocket client
+function writeMessage(message) {
+    var received = document.getElementById("received");
+    var br = document.createElement("BR");
+    var text = document.createTextNode(message);
+    received.appendChild(br);
+    received.appendChild(text);
+}
+
+
+function connect(host, port) {
+    // Create WebSocket connection.
+    var ws = new WebSocket('ws://' + host + ':' + port);
+
+    // Connection opened
+    ws.onopen = function () {
+        console.log('Connected to server');
+        // alert message
+        appendAlert('Connected to server', 'success');
+        // wait for 1.5 seconds and remove the alert message and close the modal
+        setTimeout(() => {
+            alertPlaceholder.innerHTML = '';
+            $('#configModal').modal('hide');
+        }, 1500);
+    };
+    ws.onmessage = function (event) {
+        console.log('Message from server:', event.data);
+        // write the message to the received div
+        writeMessage(event.data);
+    };
+    ws.onclose = function () {
+        console.log('Disconnected from server');
+        if (!$('#configModal').hasClass('show')) {
+            // write the message to the received div
+            writeMessage('Disconnected from server');
+        }  
+    };
+    ws.onerror = function (event) {
+        console.error('WebSocket error observed:', event);
+        // if modal is open
+        if ($('#configModal').hasClass('show')) {
+            // alert message
+            appendAlert('Cannot connect to server', 'danger');
+        } else {
+            // write the message to the received div
+            writeMessage('WebSocket error observed');
+        }
+    };
+}
